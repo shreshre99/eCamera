86407c9eaddac472f7bb859c6476072ee02ca331
Shreyas Kulkarni <shreabhi.kulkarni@gmail.com>
2019-02-15 12:54:23 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function cameraStart() {
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraSensor.getContext("3d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
