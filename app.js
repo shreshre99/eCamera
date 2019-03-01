673c00b12a84f68a682827fd858084cd1ed51ea7
Shreyas Kulkarni <srk37@duke.edu>
2019-03-01 16:35:11 -0500

    zoom

--- app.js
+++ app.js
@@ -33,22 +33,6 @@ const cameraView = document.querySelector("#camera--view"),
       cameraSensor = document.querySelector("#camera--sensor"),
       cameraTrigger = document.querySelector("#camera--trigger")
 
-if(controls){
-    controls.innerHTML =  '<button class="play">play</button>'+
-                          '<div id="change">' +
-                            '<button class="zoomin">+</button>' +
-                            '<button class="zoomout">-</button>' +
-                            '<button class="left">⇠</button>' +
-                            '<button class="right">⇢</button>' +
-                            '<button class="up">⇡</button>' +
-                            '<button class="down">⇣</button>' +
-                            '<button class="rotateleft">&#x21bb;</button>' +
-                            '<button class="rotateright">&#x21ba;</button>' +
-                            '<button class="reset">reset</button>' +
-                          '</div>';
-  }
-
-
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -56,73 +40,6 @@ function cameraStart() {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
 
-
-          controls.addEventListener('click',function(e){
-    t = e.target;
-    if(t.nodeName.toLowerCase()==='button'){
-
-/* Check the class name of the button and act accordingly */    
-      switch(t.className){
-
-/* Toggle play functionality and button label */    
-        case 'play':
-          if(v.paused){
-            v.play();
-            t.innerHTML = 'pause';
-          } else {
-            v.pause();
-            t.innerHTML = 'play';
-          }
-        break;
-
-/* Increase zoom and set the transformation */
-        case 'zoomin':
-          zoom = zoom + 0.1;
-          v.style[prop]='scale('+zoom+') rotate('+rotate+'deg)';
-        break;
-
-/* Decrease zoom and set the transformation */
-        case 'zoomout':
-          zoom = zoom - 0.1;
-          v.style[prop]='scale('+zoom+') rotate('+rotate+'deg)';
-        break;
-
-/* Increase rotation and set the transformation */
-        case 'rotateleft':
-          rotate = rotate + 5;
-          v.style[prop]='rotate('+rotate+'deg) scale('+zoom+')';
-        break;
-/* Decrease rotation and set the transformation */
-        case 'rotateright':
-          rotate = rotate - 5;
-          v.style[prop]='rotate('+rotate+'deg) scale('+zoom+')';
-        break;
-
-/* Move video around by reading its left/top and altering it */
-        case 'left':
-          v.style.left = (parseInt(v.style.left,10) - 5) + 'px';
-        break;
-        case 'right':
-          v.style.left = (parseInt(v.style.left,10) + 5) + 'px';
-        break;
-        case 'up':
-          v.style.top = (parseInt(v.style.top,10) - 5) + 'px';
-        break;
-        case 'down':
-          v.style.top = (parseInt(v.style.top,10) + 5) + 'px';
-        break;
-
-/* Reset all to default */
-        case 'reset':
-          zoom = 1;
-          rotate = 0;
-          v.style.top = 0 + 'px';
-          v.style.left = 0 + 'px';
-          v.style[prop]='rotate('+rotate+'deg) scale('+zoom+')';
-        break;
-      } 
-
-  }
 
 
 
