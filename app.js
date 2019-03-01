3d6b3b2316cf090619a2870e87eb9f6dde59ff66
Shreyas Kulkarni <srk37@duke.edu>
2019-03-01 13:50:38 -0500

    zoom

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ navigator.mediaDevices.getUserMedia({ video: true })
             return;
         }track.applyConstraints({advanced : [{zoom: zoom.value}] });
 
-        let cameraView.srcObject = new ImageCapture(track);
+        cameraView.srcObject = new ImageCapture(track);
 
 
 })
