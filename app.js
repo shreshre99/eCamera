b65d2471e4b6b935ac6ad5fbfb0a16d6e6a02b34
Shreyas Kulkarni <srk37@duke.edu>
2019-03-03 17:10:44 -0500

    zoom

--- app.js
+++ app.js
@@ -2,30 +2,6 @@
 
 var constraints = { video: { deviceId: "37acb87543cb1690b5859a5372d2dabd6c9318cc5916505c584fe0c58d847569" }, audio: false };
 
-var zoom = 1, 
-    rotate = 0; 
-
-
-var stage = document.getElementById('stage'),
-    v = document.getElementsByTagName('video')[0],
-    controls = document.getElementById('controls');
-
-
-var properties = ['transform', 'WebkitTransform', 'MozTransform',
-                 'msTransform', 'OTransform'],
-    prop = properties[0];
-
-/* Iterators and stuff */    
-var i,j,t;
-  
-/* Find out which CSS transform the browser supports */
-for(i=0,j=properties.length;i<j;i++){
-    if(typeof stage.style[properties[i]] !== 'undefined'){
-      prop = properties[i];
-      break;
-    }
-} 
-
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
