e4c15c2ea7f4ce07ea075e161ac73692237efc4a
Shreyas Kulkarni <srk37@duke.edu>
2019-03-10 13:34:20 -0400

    ewfwef

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 //WORK ON COMING UP WITH SCRIPT TO FIND OUT THE DEVICEid
-
-var constraints = { video: { deviceId: "8B8AD18E-2C5E-F9F9-7D35-311F363872B8" }, audio: false };
-var zoom = 1;
-
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
@@ -10,6 +6,29 @@ const cameraView = document.querySelector("#camera--view"),
     zoomIn = document.querySelector("#ZoomIn"),
     zoomOut = document.querySelector("#ZoomOut")
 // Access the device camera and stream to cameraView
+
+var DEVICEID = 0; 
+
+navigator.mediaDevices.enumerateDevices()
+.then(function(devices) {
+  devices.forEach(function(device) {
+    if(device.label === "USB2.0 Camera (eb1a:299f)"){
+        DEVICEID = device.deviceId; 
+
+    }
+    else{ 
+        DEVICEID = "B6A9250F-23F7-DCD0-F5D8-A893C0BCB6DA"
+    }
+  });
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
+
+
+var constraints = { video: { deviceId: DEVICEID }, audio: false };
+var zoom = 1;
+
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -36,3 +55,10 @@ zoomOut.onclick = function() {
 }
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+
+
+/*Third party cloud based systems - redcap (duke used to have it) → data transfer system where you can save/upload/pull from the database system that they already have 
+Firebase (if not encrypted) multiple cloud → webapp development platform, nodejs SDK, NPM bundler 
+Appjs → building desktop applications (just HTML, nodejs, etc) ---> if nodejs, it should be able to incorporate API’s 
+*/
